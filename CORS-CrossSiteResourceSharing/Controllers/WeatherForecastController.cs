195a3e7f37d2a7aa4ec7ed0544bbb29305913a41
Barış Avtan <baris.avtan@arvento.com>
2022-08-25 22:22:14 +0300

    Add project files.

---
+++ CORS-CrossSiteResourceSharing/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CORS_CrossSiteResourceSharing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [EnableCors("AllowSites")] //Bunu controller seviyesinde de uygulayabiliriz direk controller üzerine yazılabilir.
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+     
+        [DisableCors] //öreneğin controller ın tepesine bir enablecors yazadın fakat her methodda çalışmasını istemiyorsan ozaman metot başına  DisableCors yaz
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ CORS-CrossSiteResourceSharing/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//EXAMPLE 1
+
+//builder.Services.AddCors(opts =>
+//{
+//    opts.AddDefaultPolicy(builder =>
+//    {
+//        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();//Hangi orjinden gelirse gelsin,header'ı ne olursa olsun, metodu ne olursa olsun get veya post hepisini kabul et!
+
+//    });
+//});
+
+//EXAMPLE 2
+
+string ApiCorsPolicy = "AllowSites";
+
+//builder.Services.AddCors(opts =>
+//{
+//    opts.AddPolicy(ApiCorsPolicy, builder =>
+//    {
+//        builder.WithOrigins("https://localhost:7159/", "https://localhost:1010/").AllowAnyHeader().AllowAnyMethod();
+//    });
+//});
+
+
+//builder.Services.AddCors(options => options.AddPolicy(name: "AllowSites", builder => {
+//    builder.WithOrigins("https://localhost:7159/", "https://localhost:1010/").AllowAnyHeader().AllowAnyMethod();
+//}));
+
+
+builder.Services.AddCors(options =>
+{
+    //1- sadece https://localhost:7159", "http://localhost:4200" bu urlleden istek gelebilir bunun dısındaki urllerden istek  gelirse kabul etmez.
+    options.AddPolicy(name: "AllowSites",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:7159", "http://localhost:4200")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+
+
+    //2-bu urllerden istek gelebilir ve geldiğinde headercontent type 'ı x-custom-header olmalı.
+    options.AddPolicy(name: "AllowSites2",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:1010", "http://localhost:1212").WithHeaders(HeaderNames.ContentType, "x-custom-header");
+
+        });
+
+
+    //3-sadece subdomainlerden gelen istekler için
+
+    options.AddPolicy(name: "SubdomainPolicy",
+    builder =>
+    {
+        builder.WithOrigins("https//*.arv.com", "https//*.example.com").SetIsOriginAllowedToAllowWildcardSubdomains().AllowAnyHeader().AllowAnyMethod();
+
+    });
+
+    //4-sadece get ve post metodlarına izin verme
+    options.AddPolicy(name: "UseGetPostMethod",
+    builder =>
+    {
+     builder.WithOrigins("https//example.com").WithMethods("POST", "GET").AllowAnyHeader();
+
+    });
+
+    //app.UseCors(); parantez içersinde hiç birşey belirtilmezse uyguluma içinde istedigin policiy name i kullanabilirsin ama isim yazarsan sadece o policy çalışacaktır.
+
+
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+////EXAMPLE 2
+//app.UseCors("AllowSites");
+
+//EXAMPLE 1
+app.UseCors();//default policy yani kurallar (yani yukarda yazdıgımız) usercors() içine bişey yazılmaz
+//[EnableCors("AllowSites")] controller üstüne eklendi.Dikkat ! 
+//[HttpGet(Name = "GetWeatherForecast")]
+
+app.MapControllers();
+
+app.Run();
